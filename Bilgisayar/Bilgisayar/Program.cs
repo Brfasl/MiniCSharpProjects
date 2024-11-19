419e4089c0c317b14074d189a7061a75421ad384
Berfin <152812694+Brfasl@users.noreply.github.com>
2024-11-20 01:21:57 +0300

    Add files via upload

---
+++ Bilgisayar/Bilgisayar/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class Islemci
+{
+    public int Cekirdekler { get; set; }
+    public double Frekans { get; set; }
+
+    public Islemci(int cekirdekler, double frekans)
+    {
+        Cekirdekler = cekirdekler;
+        Frekans = frekans;
+    }
+
+    public string IslemciBilgi()
+    {
+        return $"{Cekirdekler} Çekirdek, {Frekans} GHz";
+    }
+}
+
+public class Bilgisayar
+{
+    public string Model { get; set; }
+    public Islemci Islemci { get; set; }
+
+    public Bilgisayar(string model, Islemci islemci)
+    {
+        Model = model;
+        Islemci = islemci;
+    }
+
+    public string BilgisayarBilgi()
+    {
+        return $"{Model} - {Islemci.IslemciBilgi()}";
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Islemci islemci = new Islemci(8, 3.5);
+        Bilgisayar bilgisayar = new Bilgisayar("Lenovo ThinkPad", islemci);
+
+        Console.WriteLine(bilgisayar.BilgisayarBilgi());
+        Console.Read();
+    }
+}
---
+++ Ebeveyn/Ebeveyn/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Ebeveyn
+{
+    public string Ad { get; set; }
+    public List<Cocuk> Cocuklar { get; set; }
+
+    public Ebeveyn(string ad)
+    {
+        Ad = ad;
+        Cocuklar = new List<Cocuk>();
+    }
+
+    public void CocukEkle(Cocuk cocuk)
+    {
+        Cocuklar.Add(cocuk);
+        cocuk.EbeveynAtama(this); 
+    }
+}
+
+class Cocuk
+{
+    public string Ad { get; set; }
+    public Ebeveyn Ebeveyn { get; private set; }
+
+    public Cocuk(string ad)
+    {
+        Ad = ad;
+    }
+
+    public void EbeveynAtama(Ebeveyn ebeveyn)
+    {
+        Ebeveyn = ebeveyn;
+        if (!ebeveyn.Cocuklar.Contains(this))
+        {
+            ebeveyn.CocukEkle(this); 
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Ebeveyn ebeveyn1 = new Ebeveyn("Mehmet");
+        Cocuk cocuk1 = new Cocuk("Ahmet");
+
+        ebeveyn1.CocukEkle(cocuk1);
+
+        Console.WriteLine($"{ebeveyn1.Ad} adlı ebeveynin çocukları:");
+        foreach (var cocuk in ebeveyn1.Cocuklar)
+        {
+            Console.WriteLine($"- {cocuk.Ad}");
+        }
+
+        Console.WriteLine($"\n{cocuk1.Ad} adlı çocuğun ebeveyni: {cocuk1.Ebeveyn.Ad}");
+        Console.Read();
+    }
+}
