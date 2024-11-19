07de5cacc8a1089569cbea3ee82a6cda10df989f
Berfin <152812694+Brfasl@users.noreply.github.com>
2024-11-20 01:23:47 +0300

    Add files via upload

---
+++ Kitaplar/Kitaplar/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Yazar
+{
+    public string Ad { get; set; }
+    public string Ülke { get; set; }
+    public List<Kitap> Kitaplar { get; set; }
+
+    public Yazar(string ad, string ülke)
+    {
+        Ad = ad;
+        Ülke = ülke;
+        Kitaplar = new List<Kitap>();
+    }
+
+    public void KitapEkle(Kitap kitap)
+    {
+        Kitaplar.Add(kitap);
+        kitap.YazarAtama(this); 
+    }
+}
+
+class Kitap
+{
+    public string Baslik { get; set; }
+    public DateTime YayinTarihi { get; set; }
+    public Yazar Yazar { get; private set; }
+
+    public Kitap(string baslik, DateTime yayinTarihi)
+    {
+        Baslik = baslik;
+        YayinTarihi = yayinTarihi;
+    }
+
+    public void YazarAtama(Yazar yazar)
+    {
+        Yazar = yazar;
+        if (!yazar.Kitaplar.Contains(this))
+        {
+            yazar.KitapEkle(this);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Yazar yazar1 = new Yazar("Orhan Pamuk", "Türkiye");
+        Kitap kitap1 = new Kitap("Masumiyet Müzesi", new DateTime(2008, 8, 1));
+
+        yazar1.KitapEkle(kitap1);
+
+        Console.WriteLine($"{yazar1.Ad} adlı yazarın kitapları:");
+        foreach (var kitap in yazar1.Kitaplar)
+        {
+            Console.WriteLine($"- {kitap.Baslik} ({kitap.YayinTarihi.Year})");
+        }
+
+        Console.WriteLine($"\n{kitap1.Baslik} kitabının yazarı: {kitap1.Yazar.Ad}");
+        Console.Read();
+    }
+}
---
+++ Okullar/Okullar/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Ogrenci
+{
+    public string Ad { get; set; }
+    public int Yas { get; set; }
+
+    public Ogrenci(string ad, int yas)
+    {
+        Ad = ad;
+        Yas = yas;
+    }
+
+    public string OgrenciBilgi()
+    {
+        return $"{Ad}, {Yas} yaşında";
+    }
+}
+
+public class Okul
+{
+    public string Ad { get; set; }
+    public List<Ogrenci> Ogrenciler { get; set; } = new List<Ogrenci>();
+
+    public Okul(string ad)
+    {
+        Ad = ad;
+    }
+
+    public void OgrenciEkle(Ogrenci ogrenci)
+    {
+        Ogrenciler.Add(ogrenci);
+    }
+
+    public string OkulBilgi()
+    {
+        return $"{Ad} okulunda {Ogrenciler.Count} öğrenci var.";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Okul okul = new Okul("Anadolu Lisesi");
+        okul.OgrenciEkle(new Ogrenci("Ali", 16));
+        okul.OgrenciEkle(new Ogrenci("Ayşe", 17));
+
+        Console.WriteLine(okul.OkulBilgi());
+        Console.Read();
+    }
+}
