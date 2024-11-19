41b07b681bcdd1f670593e30d5b1614a4dd7b825
Berfin <152812694+Brfasl@users.noreply.github.com>
2024-11-20 01:15:38 +0300

    Add files via upload

---
+++ Author/Author/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Yazar
+{
+    public string Ad { get; set; }
+    public string Ulke { get; set; }
+    public List<Kitap> Kitaplar { get; set; }
+
+    public Yazar(string ad, string ulke)
+    {
+        Ad = ad;
+        Ulke = ulke;
+        Kitaplar = new List<Kitap>();
+    }
+
+    public void KitapEkle(Kitap kitap)
+    {
+        Kitaplar.Add(kitap);
+    }
+
+    public void BilgiYazdir()
+    {
+        Console.WriteLine($"Yazar Adı: {Ad}, Ülke: {Ulke}");
+        Console.WriteLine("Kitapları:");
+        foreach (var kitap in Kitaplar)
+        {
+            Console.WriteLine($"- {kitap.Baslik}, ISBN: {kitap.ISBN}");
+        }
+    }
+}
+
+class Kitap
+{
+    public string Baslik { get; set; }
+    public string ISBN { get; set; }
+
+    public Kitap(string baslik, string isbn)
+    {
+        Baslik = baslik;
+        ISBN = isbn;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Yazar yazar1 = new Yazar("Orhan Pamuk", "Türkiye");
+        Kitap kitap1 = new Kitap("Kırmızı Saçlı Kadın", "1234567890");
+        Kitap kitap2 = new Kitap("Benim Adım Kırmızı", "0987654321");
+
+        yazar1.KitapEkle(kitap1);
+        yazar1.KitapEkle(kitap2);
+
+        yazar1.BilgiYazdir();
+        Console.Read();
+    }
+}
---
+++ Calisan/Calisan/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calisan
+{
+    public class Departman
+    {
+        public string Ad { get; set; }
+        public string Lokasyon { get; set; }
+
+        public Departman(string ad,string lokasyon)
+        {
+            Ad = ad;
+            Lokasyon = lokasyon;
+            
+        }
+    }
+    public class Calisan
+    {
+        public string Ad { get; set; }
+        public string Pozisyon { get; set; }
+        public Departman Departman { get; set; }
+        public Calisan(string ad,string pozisyon)
+        {
+            Ad = ad;
+            Pozisyon = pozisyon;
+        }
+        public void DepartmanAtama(Departman departman)
+        {
+            Departman = departman;
+        }
+       
+        public void BilgiGöster()
+        {
+            Console.WriteLine($"Çalışan Adı: {Ad}, Pozisyon: {Pozisyon}");
+            if(Departman != null)
+            {
+                Console.WriteLine($"Departman:{Departman.Ad} ,Lokasyon:{Departman.Lokasyon}");
+            }
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisan c1 = new Calisan("Ali","Müdür");
+            Departman d1 = new Departman("ID", "İstanbul");
+            c1.DepartmanAtama(d1);
+            c1.BilgiGöster();
+            Console.Read();
+        }
+    }
+}
